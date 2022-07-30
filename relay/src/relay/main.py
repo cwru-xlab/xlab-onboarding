bd6fb1521663c2f7fbae4aaac6c6cbcd19c0bf08
rtatton <rdt17@protonmail.com>
2022-07-30 18:23:06 -0400

    Remove env_file from db
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- relay/src/relay/main.py
+++ relay/src/relay/main.py
@@ -1,8 +1,10 @@
-# ses = boto3.resource("sesv2")
+import boto3
+
+ses = boto3.resource("sesv2")
 
 
-# def relay(address: str, email: str):
-#     # https://boto3.amazonaws.com/v1/documentation/api/latest/reference
-#     # /services/sesv2.html#SESV2.Client.send_email
-#     ses.send_email()
-#     pass
+def relay(address: str, email: str):
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference
+    # /services/sesv2.html#SESV2.Client.send_email
+    ses.send_email()
+    pass
