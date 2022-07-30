a15c84f1dc993bd5000b93537edd3849b4fba3a4
rtatton <rdt17@protonmail.com>
2022-07-30 17:54:58 -0400

    API complete; needs testing
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -2,66 +2,85 @@ from __future__ import annotations
 
 from typing import Optional
 
+import boto3
 import fastapi
-from pydantic import constr
-from redis_om.model import model
+from aredis_om import model
+from aws_error_utils import errors
+from fastapi import responses, status
+from pydantic import EmailStr, constr
 
-from util import migrate
+import settings
+import util
 
-api = fastapi.FastAPI()
+api = fastapi.FastAPI(default_response_class=responses.ORJSONResponse)
+ses = boto3.resource("sesv2")
 
 
-@migrate
+@util.migrate
 class User(model.HashModel):
     username: constr(regex=r"[\w\-.]+", strict=True) = model.Field(index=True)
+    recovery_email: EmailStr
 
     @classmethod
-    def create(cls, username: str, save: bool = True, **kwargs) -> User:
-        if not (user := cls.lookup(username)):
-            user = User(username=username, **kwargs)
-            if save:
-                user.save()
-        return user
-
-    @classmethod
-    def destroy(cls, username: str) -> None:
-        if user := cls.lookup(username):
-            User.delete(user.pk)
-
-    @classmethod
-    def lookup(cls, username: str) -> Optional[User]:
-        match = User.find(User.username == username).all()
+    async def lookup(cls, username: str) -> Optional[User]:
+        match = await User.find(User.username == username).all()
         return None if not match else match[0]
 
 
-@api.get("/")
-def read_root():
-    return {"Hello": "World"}
+@api.get("/users/{username}", status_code=status.HTTP_200_OK)
+async def get_user(username: str) -> Optional[User]:
+    if not (user := await User.lookup(username)):
+        raise_user_not_found(username)
+    return user
 
 
-@api.get("/user/{username}")
-def test_redis(username: str):
-    return User.create(username)
-
-# ses = boto3.resource("sesv2")
-
-
-# class User(model.HashModel):
-#     username: StrictStr = model.Field(index=True, regex=r"[\w\-\.]+")
-#     recovery_email: EmailStr
-
-# def add_new_user(username: str, recovery_email: str) -> None:
-#     if User.find(User.username == username).all():
-#         raise ValueError("Username already exists")
-#     else:
-#         User(username=username, recovery_email=recovery_email).save()
+@api.post("/users/", status_code=status.HTTP_201_CREATED)
+async def create_user(user: User) -> None:
+    if not User.lookup(user.username):
+        await User.save(user)
+    else:
+        raise_user_exists(user.username)
 
 
-# def request_new_email(username: str) -> str:
-#     # https://boto3.amazonaws.com/v1/documentation/api/latest/reference
-#     # /services/sesv2.html#SESV2.Client.create_email_identity
-#     try:
-#         address = f"{username}@{settings.config.domain}"
-#         response = ses.create_email_identity(address)
-#     except exceptions.ClientError:
-#         pass
+@api.delete("/users/{username}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(username: str) -> None:
+    if user := await User.lookup(username):
+        await User.delete(user.pk)
+    else:
+        raise_user_not_found(username)
+
+
+def raise_user_not_found(username: str) -> None:
+    raise fastapi.HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"User '{username}' not found")
+
+
+def raise_user_exists(username: str) -> None:
+    raise fastapi.HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail=f"User '{username}' already exists")
+
+
+@api.post("/emails/", status_code=status.HTTP_201_CREATED)
+def create_email(username: str) -> str:
+    address = f"{username}@{settings.config.domain}"
+    try:
+        ses.create_email_identity(address)
+        waiter = ses.get_waiter("identity_exists")
+        waiter.wait(
+            Identities=[address],
+            WaiterConfig={"Delay": 3, "MaxAttempts": 20})
+    except (errors.AlreadyExistsException,
+            errors.LimitExceededException,
+            errors.TooManyRequestsException,
+            errors.BadRequestException,
+            errors.ConcurrentModificationException,
+            errors.NotFoundException) as e:
+        raise_aws_exception(e)
+    return address
+
+
+def raise_aws_exception(error) -> None:
+    raise fastapi.HTTPException(
+        status_code=error.http_status_code, detail=error.message)
