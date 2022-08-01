815388cd2f53c981233943d76b8c21b8d31bed21
rtatton <rdt17@protonmail.com>
2022-08-01 15:55:43 -0400

    Add web project
    - Initial copy from Jinja Bootstrap 5 Volt.
    - Refactorings based on xmail to use pydantic
    - Source: https://appseed.us/product/volt-dashboard/jinja/
    
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- web/src/web/main.py
+++ web/src/web/main.py
@@ -1,8 +1,24 @@
-from flask import Flask
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
 
-app = Flask(__name__)
+import flask
+import jinja2
+
+import settings
+
+app = flask.Flask(__name__)
+settings.init_app(app)
 
 
-@app.route("/")
-def hello():
-    return "Hello World from Flask"
+@app.route("/", defaults={"path": "index.html"})
+@app.route("/<path:path>")
+def index(path: str):
+    try:
+        # Detect the current page
+        segment = flask.request.path.split("/")[-1] or "index"
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        return flask.render_template("home/" + path, segment=segment)
+    except jinja2.TemplateNotFound:
+        return flask.render_template("home/page-404.html"), 404
---
+++ web/src/web/settings.py
@@ -0,0 +1,46 @@
+import hat
+import pydantic
+from flask import Flask
+from keyring.credentials import Credential
+from pydantic import DirectoryPath, SecretStr, StrictStr
+
+HAT_CLIENT_KEY = "HAT_CLIENT"
+ASSETS_ROOT_KEY = "ASSETS_ROOT"
+
+
+def init_app(app: Flask) -> None:
+    config = Settings()
+    app.config[HAT_CLIENT_KEY] = config.hat_client
+    app.config[ASSETS_ROOT_KEY] = config.assets_root
+
+
+class Settings(pydantic.BaseSettings):
+    hat_username: StrictStr
+    hat_password: SecretStr
+    hat_namespace: StrictStr
+    assets_root: DirectoryPath
+
+    class Config:
+        allow_mutation = False
+
+    @property
+    def hat_client(self) -> hat.HatClient:
+        token = hat.ApiOwnerToken(self.hat_credential())
+        return hat.HatClient(token)
+
+    def hat_credential(self) -> Credential:
+        class HatCredential(Credential):
+            __slots__ = "_settings"
+
+            def __init__(self, settings: Settings):
+                self._settings = settings
+
+            @property
+            def username(self) -> str:
+                return self._settings.hat_username
+
+            @property
+            def password(self) -> str:
+                return self._settings.hat_password.get_secret_value()
+
+        return HatCredential(self)
