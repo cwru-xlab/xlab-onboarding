10d095a1addeecfb817b22ab9417c663c5abf9ca
rtatton <rdt17@protonmail.com>
2022-08-02 00:39:52 -0400

    Add app factory and factory.py
    Signed-off-by: rtatton <rdt17@protonmail.com>

---
+++ web/src/web/factory.py
@@ -0,0 +1,38 @@
+import flask
+import jinja2
+from hat import HatClient
+
+import settings
+
+
+def make_app() -> flask.Flask:
+    app = flask.Flask(__name__, instance_relative_config=True)
+    app.teardown_appcontext(_teardown_appcontext)
+    with app.app_context():
+        settings.init_app(app)
+        _ensure_instance_folder_exists()
+
+    @app.route("/", defaults={"path": "index.html"})
+    @app.route("/<path:path>")
+    def index(path: str):
+        try:
+            current_page = flask.request.path.split("/")[-1] or "index"
+            return flask.render_template("home/" + path, segment=current_page)
+        except jinja2.TemplateNotFound:
+            return flask.render_template("home/page-404.html"), 404
+
+    return app
+
+
+def _teardown_appcontext(exception: BaseException) -> None:
+    hat_client: HatClient = flask.g.pop(settings.HAT_CLIENT_KEY, None)
+    if hat_client is not None:
+        hat_client.close()
+
+
+def _ensure_instance_folder_exists() -> None:
+    import os
+    try:
+        os.makedirs(flask.current_app.instance_path)
+    except OSError:
+        pass
--- web/src/web/main.py
+++ web/src/web/main.py
@@ -3,20 +3,6 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-import flask
-import jinja2
+import factory
 
-import settings
-
-app = flask.Flask(__name__)
-settings.init_app(app)
-
-
-@app.route("/", defaults={"path": "index.html"})
-@app.route("/<path:path>")
-def index(path: str):
-    try:
-        current_page = flask.request.path.split("/")[-1] or "index"
-        return flask.render_template("home/" + path, segment=current_page)
-    except jinja2.TemplateNotFound:
-        return flask.render_template("home/page-404.html"), 404
+app = factory.make_app()
