d805f7c3ed209d13ffe7f5d5c3a82352305ea7f6
rtatton <rdt17@protonmail.com>
2022-08-02 00:21:57 -0400

    Add bcrypt and password hashing
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- api/src/api/util.py
+++ api/src/api/util.py
@@ -1,6 +1,7 @@
 import functools
-from typing import TypeVar
+from typing import AnyStr, TypeVar
 
+import bcrypt
 import redis_om
 
 T = TypeVar("T", bound=redis_om.RedisModel)
@@ -13,3 +14,15 @@ def migrate(model: T) -> T:
         return model
 
     return migrated()
+
+
+def hash_pwd(pwd: AnyStr, rounds: int = 12) -> bytes:
+    return bcrypt.hashpw(_encode(pwd), bcrypt.gensalt(rounds))
+
+
+def check_pwd(pwd: AnyStr, hashed: bytes) -> bool:
+    return bcrypt.checkpw(_encode(pwd), hashed)
+
+
+def _encode(s: AnyStr) -> bytes:
+    return s if isinstance(s, bytes) else s.encode("utf-8")
