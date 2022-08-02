ccdf421e88eb6dda0e8728ef9052a50e6f7f8bff
rtatton <rdt17@protonmail.com>
2022-08-02 14:07:46 -0400

    Add with_fallback decorator
    Considering using Flask-Security-Too, where I will want to decorate specific routes with @auth_required, but still have the same default fallback logic to a 400 page.
    
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- web/src/web/factory.py
+++ web/src/web/factory.py
@@ -1,3 +1,6 @@
+import functools
+from typing import Callable
+
 import flask
 import jinja2
 from hat import HatClient
@@ -7,23 +10,32 @@ import settings
 
 def make_app() -> flask.Flask:
     app = flask.Flask(__name__)
-    app.teardown_appcontext(_teardown_appcontext)
+    app.teardown_appcontext(teardown_appcontext)
     with app.app_context():
         settings.init_app(app)
 
     @app.route("/", defaults={"path": "index.html"})
     @app.route("/<path:path>")
+    @with_fallback
     def index(path: str):
-        try:
-            current_page = flask.request.path.split("/")[-1] or "index"
-            return flask.render_template("home/" + path, segment=current_page)
-        except jinja2.TemplateNotFound:
-            return flask.render_template("home/404.html"), 404
+        current_page = flask.request.path.split("/")[-1] or "index"
+        return flask.render_template("home/" + path, segment=current_page)
 
     return app
 
 
-def _teardown_appcontext(exception: BaseException) -> None:
+def with_fallback(render) -> Callable[[...], str]:
+    @functools.wraps(render)
+    def wrapped(*args, **kwargs):
+        try:
+            return render(*args, **kwargs)
+        except jinja2.TemplateNotFound:
+            return flask.render_template("home/404.html"), 404
+
+    return wrapped
+
+
+def teardown_appcontext(exception: BaseException) -> None:
     hat_client: HatClient = flask.g.pop("hat_client", None)
     if hat_client is not None:
         hat_client.close()
