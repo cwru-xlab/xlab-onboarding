3d0d986bcbb4329fa2b83fd017d5ae87f57c0b9f
rtatton <rdt17@protonmail.com>
2022-08-02 00:52:21 -0400

    Remove instance-relative config
    Unnecessary
    
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- web/src/web/factory.py
+++ web/src/web/factory.py
@@ -1,5 +1,3 @@
-import os
-
 import flask
 import jinja2
 from hat import HatClient
@@ -8,11 +6,10 @@ import settings
 
 
 def make_app() -> flask.Flask:
-    app = flask.Flask(__name__, instance_relative_config=True)
+    app = flask.Flask(__name__)
     app.teardown_appcontext(_teardown_appcontext)
     with app.app_context():
         settings.init_app(app)
-        _ensure_instance_folder_exists()
 
     @app.route("/", defaults={"path": "index.html"})
     @app.route("/<path:path>")
@@ -30,10 +27,3 @@ def _teardown_appcontext(exception: BaseException) -> None:
     hat_client: HatClient = flask.g.pop("hat_client", None)
     if hat_client is not None:
         hat_client.close()
-
-
-def _ensure_instance_folder_exists() -> None:
-    try:
-        os.makedirs(flask.current_app.instance_path)
-    except OSError:
-        pass
