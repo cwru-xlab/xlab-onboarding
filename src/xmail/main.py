420c5162e8358b78b0e6cbb24c83431c0d687d9b
rtatton <rdt17@protonmail.com>
2022-08-22 15:06:06 -0400

    Simplify main.py
    Signed-off-by: rtatton <rdt17@protonmail.com>

--- src/xmail/main.py
+++ src/xmail/main.py
@@ -1,7 +1,3 @@
 import factory
 
-# Ref: https://stackoverflow.com/a/51397334
-if __name__ == '__main__':
-    factory.make_app().run()
-else:
-    app = factory.make_app()
+app = factory.make_app()
